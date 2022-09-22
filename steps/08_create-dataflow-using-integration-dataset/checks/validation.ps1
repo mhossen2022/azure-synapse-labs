0e0b2a52390c06571f338abdec05d9293b6578b1
vAyushkhare <110239762+vAyushkhare@users.noreply.github.com>
2022-09-22 15:27:32 +0530

    Update validation.ps1

--- steps/08_create-dataflow-using-integration-dataset/checks/validation.ps1
+++ steps/08_create-dataflow-using-integration-dataset/checks/validation.ps1
@@ -1,4 +1,9 @@
 param(
     [string]
-    $Resourcegroupname
+    $WorkSpacename
 )
+
+  $df = Get-AzSynapseDataFlow -WorkspaceName $WorkSpacename -Name adworks_DF
+  if ($df.Name -eq "adworks_DF") { 
+    Write-Host "Data flow adworks_DF created" }
+else { Write-Host "Data flow adworks_DF not created" }
