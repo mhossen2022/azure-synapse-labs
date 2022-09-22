d0dc99ca773a2133697c41beba68c67cb53ff630
vAyushkhare <110239762+vAyushkhare@users.noreply.github.com>
2022-09-22 17:35:09 +0530

    Update validation.ps1

--- steps/05_NoteBook-to-create-ParquetFile-inside-ADLSGen2-Storage-using-OpenDataSet/checks/05_NoteBook-to-create-ParquetFile-inside-ADLSGen2-Storage-using-OpenDataSet/validation.ps1
+++ steps/05_NoteBook-to-create-ParquetFile-inside-ADLSGen2-Storage-using-OpenDataSet/checks/05_NoteBook-to-create-ParquetFile-inside-ADLSGen2-Storage-using-OpenDataSet/validation.ps1
@@ -1,4 +1,14 @@
 param(
     [string]
-    $Resourcegroupname
+    $WorkSpacename
 )
+
+$notebook = Get-AzSynapseNotebook -WorkspaceName $WorkSpacename -Name ntb_Open_DataSet_To_ADLS
+if ($notebook)
+{
+    Write-Host "notebook ntb_Open_DataSet_To_ADLS created"
+}
+else
+{
+    Write-Host "notebook ntb_Open_DataSet_To_ADLS not created"
+}
