8961798eb141da21c67019684e74d5d87f96ba86
vAyushkhare <110239762+vAyushkhare@users.noreply.github.com>
2022-09-27 18:03:19 +0530

    Add files via upload

---
+++ steps/02_data_transformation_using_Spark/checks/05_NoteBook-to-create-ParquetFile-inside-ADLSGen2-Storage-using-OpenDataSet/validation.ps1
@@ -0,0 +1,16 @@
+param(
+    [string]
+    $Resourcegroupname
+)
+$WorkSpace = Get-AzResource -ResourceGroupName $Resourcegroupname -Resourcetype Microsoft.Synapse/workspaces
+$WorkSpacename = $WorkSpace.Name
+
+$notebook = Get-AzSynapseNotebook -WorkspaceName $WorkSpacename -Name ntb_Open_DataSet_To_ADLS
+if ($notebook)
+{
+    Write-Host "notebook ntb_Open_DataSet_To_ADLS created"
+}
+else
+{
+    Write-Host "notebook ntb_Open_DataSet_To_ADLS not created"
+}
