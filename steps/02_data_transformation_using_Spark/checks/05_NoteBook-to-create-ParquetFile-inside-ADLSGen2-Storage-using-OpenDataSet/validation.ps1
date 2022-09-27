6941292b9ca39c814e92d8cdf34dec1957960489
vAyushkhare <110239762+vAyushkhare@users.noreply.github.com>
2022-09-27 22:52:16 +0530

    Update validation.ps1

--- steps/02_data_transformation_using_Spark/checks/05_NoteBook-to-create-ParquetFile-inside-ADLSGen2-Storage-using-OpenDataSet/validation.ps1
+++ steps/02_data_transformation_using_Spark/checks/05_NoteBook-to-create-ParquetFile-inside-ADLSGen2-Storage-using-OpenDataSet/validation.ps1
@@ -14,3 +14,13 @@ else
 {
     Write-Host "notebook ntb_Open_DataSet_To_ADLS not created"
 }
+
+$notebook1 = Get-AzSynapseNotebook -WorkspaceName $WorkSpacename -Name ntb_Open_DataSet_To_LakeDB
+if ($notebook1)
+{
+    Write-Host "notebook ntb_Open_DataSet_To_LakeDB created"
+}
+else
+{
+    Write-Host "notebook ntb_Open_DataSet_To_LakeDB not created"
+}
