2914576a0c62bf8a4cedcbf3928973f8983041b0
vAyushkhare <110239762+vAyushkhare@users.noreply.github.com>
2022-10-04 15:40:26 +0530

    Update validation.ps1

--- steps/02_data-ingestion-and-transformation-using-synapse-spark/checks/02_data-ingestion-and-transformation-using-synapse-spark/validation.ps1
+++ steps/02_data-ingestion-and-transformation-using-synapse-spark/checks/02_data-ingestion-and-transformation-using-synapse-spark/validation.ps1
@@ -5,8 +5,8 @@ param(
 $WorkSpace = Get-AzResource -ResourceGroupName $Resourcegroupname -Resourcetype Microsoft.Synapse/workspaces
 $WorkSpacename = $WorkSpace.Name
 
-$notebook = Get-AzSynapseNotebook -WorkspaceName $WorkSpacename -Name ntb_Open_DataSet_To_ADLS
-if ($notebook)
+$notebook1 = Get-AzSynapseNotebook -WorkspaceName $WorkSpacename -Name ntb_Open_DataSet_To_ADLS
+if ($notebook1)
 {
     Write-Host "notebook ntb_Open_DataSet_To_ADLS created"
 }
@@ -15,12 +15,21 @@ else
     Write-Host "notebook ntb_Open_DataSet_To_ADLS not created"
 }
 
-$notebook = Get-AzSynapseNotebook -WorkspaceName $WorkSpacename -Name ntb_Open_DataSet_To_LakeDB
-if ($notebook)
+$notebook2 = Get-AzSynapseNotebook -WorkspaceName $WorkSpacename -Name ntb_Open_DataSet_To_LakeDB
+if ($notebook2)
 {
     Write-Host "notebook ntb_Open_DataSet_To_LakeDB created"
 }
 else
 {
     Write-Host "notebook ntb_Open_DataSet_To_LakeDB not created"
+}
+$notebook3 = Get-AzSynapseNotebook -WorkspaceName $WorkSpacename -Name ntb_Analyze_and_Visualize_data
+if ($notebook3)
+{
+    Write-Host "notebook ntb_Analyze_and_Visualize_data created"
+}
+else
+{
+    Write-Host "notebook ntb_Analyze_and_Visualize_data not created"
 }
